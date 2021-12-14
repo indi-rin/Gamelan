b599d4fefdb3823105e8af79a83a430ca45f5fc6
Indi Rinearson <indi.rinearson@gmail.com>
2021-12-14 13:59:27 -0800

    feat: noise when jumping

--- src/entity/Warrior.js
+++ src/entity/Warrior.js
@@ -5,6 +5,33 @@ export default class Warrior extends Phaser.Physics.Arcade.Sprite {
     super(scene, x, y, spriteKey);
     this.scene = scene;
     this.scene.add.existing(this);
-    // this.scene.physics.world.enable(this);
+    this.scene.physics.world.enable(this);
+  }
+
+  updateMovement(cursors) {
+    // Move left
+    if (cursors.left.isDown) {
+      this.setVelocityX(-360);
+    }
+    // Move right
+    else if (cursors.right.isDown) {
+      this.setVelocityX(360);
+    }
+    // Neutral (no movement)
+    else {
+      this.setVelocityX(0);
+    }
+  }
+
+  updateJump(cursors, jumpSound) {
+    if (cursors.up.isDown && this.body.touching.down) {
+      this.setVelocityY(-800);
+      jumpSound.play();
+    }
+  }
+
+  update(cursors, jumpSound) {
+    this.updateMovement(cursors);
+    this.updateJump(cursors, jumpSound);
   }
 }
--- src/scenes/FgScene.js
+++ src/scenes/FgScene.js
@@ -1,5 +1,6 @@
 import "phaser";
 import Warrior from "../entity/Warrior";
+import Platform from "../entity/Platform";
 
 export default class FgScene extends Phaser.Scene {
   constructor() {
@@ -11,23 +12,40 @@ export default class FgScene extends Phaser.Scene {
     // Preload Sprites
     // << LOAD SPRITES HERE >>
     this.load.image("warrior", "assets/sprites/warrior.png");
+    this.load.image("platform", "assets/sprites/platform.png");
     // Preload Sounds
     // << LOAD SOUNDS HERE >>
+    this.load.audio("jump", "assets/audio/demung-3.wav");
   }
 
   create() {
     // Create game entities
     // << CREATE GAME ENTITIES HERE >>
-    this.warrior = new Warrior(this, 200, 400, "warrior").setScale(0.1);
+    this.warrior = new Warrior(this, 200, 140, "warrior").setScale(0.1);
+    this.platformGroup = this.physics.add.staticGroup({ classType: Platform });
+    this.createPlatform(200, 500);
+    this.createPlatform(950, 500);
+
     // Create sounds
     // << CREATE SOUNDS HERE >>
+    this.jumpSound = this.sound.add("jump");
+
     // Create collisions for all entities
     // << CREATE COLLISIONS HERE >>
+    this.physics.add.collider(this.warrior, this.platformGroup);
+
+    // Assign cursors
+    this.cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  createPlatform(x, y) {
+    this.platformGroup.create(x, y, "platform");
   }
 
   // time: total time elapsed (ms)
   // delta: time elapsed (ms) since last update() call. 16.666 ms @ 60fps
   update(time, delta) {
     // << DO UPDATE LOGIC HERE >>
+    this.warrior.update(this.cursors, this.jumpSound);
   }
 }
